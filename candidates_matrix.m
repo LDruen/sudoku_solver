5aaab3e10e049edabeaea902ca549edb7a0b1f04
LDruen <laithdruen@gmail.com>
2022-06-06 17:41:05 -0500

    Rename candidates_matrix to candidates_matrix_3d

--- candidates_matrix.m
+++ candidates_matrix.m
@@ -1,4 +1,4 @@
-function [candidates_number_matrix,candidates_matrix] = candidates_matrix(original_matrix)
+function [candidates_number_matrix,candidates_matrix_3d] = candidates_matrix(original_matrix)
 %candidates_matrix - produces a matrix of the number of possible candidates
 %for each cell in n^2xn^2 sudoku matrix
 %
@@ -11,14 +11,14 @@ function [candidates_number_matrix,candidates_matrix] = candidates_matrix(origin
 
 row_length = length(original_matrix(1,:));
 candidates_number_matrix = zeros(row_length);
-candidates_matrix = zeros(row_length,row_length,row_length)
+candidates_matrix_3d = zeros(row_length,row_length,row_length);
 
 for i = 1:row_length
     for j = 1:row_length
         if original_matrix(i,j) == 0
             [isvalid,candidate_number,candidates] = candidate(original_matrix,i,j);
             candidates_number_matrix(i,j) = candidate_number;
-            candidates_matrix(i,j,1:length(candidates)) = candidates;
+            candidates_matrix_3d(i,j,1:length(candidates)) = candidates;
         end
     end
 end
